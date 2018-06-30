37fab565c820953a2b26cc9b6d47f5135ee369ec
Isabel Parra <isabelparra@Isabels-MacBook-Pro.local>
2018-06-30 10:52:15 -0400

    Updating HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,71 @@
-(function($){
-    $(function(){
+
+$(document).ready(function() {
+  // $('.sidenav').sidenav();
+  $('#main-container').hide();
+  $('#top-recipes').hide();
+  $('.parallax').parallax();
   
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
-  
+});
+
+$('.sidenav').sidenav({
+  menuWidth: 300,
+  edge: 'right',
+  closeOnClick: true,
+  draggable: true,
+  // onOpen: function(el)
+  // onClose: function(el)
+
+});
+// // Show sideNav
+// $('.sidenav').sidenav('show');
+// // Hide sideNav
+// $('.sidenav').sidenav('hide');
+// // Destroy sideNav
+// $('.sidenav').sidenav('destroy');
+
+$('#food-button').click(function() {
+  $('#main-container').show();
+    console.log('Choice: Food');
+    $('#buttons-start').hide();
+  });
+ 
+
+
+$('#drink-button').click(function() {
+  $('#main-container').show();
+  console.log('Choice: Drinks');
+  $('#buttons-start').hide();
+});
+
+
+
+$('#flavorize-button').click(function() {
+  $('#top-recipes').show();
+});
+
+
+
+
   $('.chips').chips();
   $('.chips-placeholder').chips({
     placeholder: 'Add ingredient...',
     secondaryPlaceholder: 'add more...',
   });
 
-  $(document).ready(function(){
 
-    // var recipes = [''];
-    $('.sidenav').sidenav();
-    // $('#top-recipes').hide();
 
-    
 
-  });
-
- 
+// $('.sidenav-trigger').Sidenav({
+//   menuWidth: 300, // Default is 240
+//   closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+//   edge: 'right',
+// }
+// );
+// $("[data-target=slide-out-r]").Sidenav({
+//   edge: 'right'
+// });
+// $(".sidenav-trigger").Sidenav({
+//   menuWidth: 300,
+//   edge: 'right',
+//   closeOnClick: true
+// });
